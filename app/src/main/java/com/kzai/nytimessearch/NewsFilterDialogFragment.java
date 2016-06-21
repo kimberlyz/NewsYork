7c6aa71f3746a18633e8c76d8f1b7a2b151890e8
Kimberly <kimberlyz621@gmail.com>
2016-06-21 13:45:44 -0700

    Spinner works

--- app/src/main/java/com/kzai/nytimessearch/NewsFilterDialogFragment.java
+++ app/src/main/java/com/kzai/nytimessearch/NewsFilterDialogFragment.java
@@ -8,15 +8,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.Calendar;
 
 /**
  * Created by kzai on 6/20/16.
  */
-public class NewsFilterDialogFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+public class NewsFilterDialogFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener, AdapterView.OnItemSelectedListener {
     
     private EditText displayDatePicker;
     
@@ -78,8 +82,18 @@ public class NewsFilterDialogFragment extends DialogFragment implements DatePick
                 mDatePicker.setTitle("Select date");
                 mDatePicker.show();  }
         });
-    }
 
+
+        Spinner spinner = (Spinner) view.findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.spinner_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
     // attach to an onclick handler to show the date picker
 
     public void showDatePickerDialog(View v) {
@@ -89,16 +103,28 @@ public class NewsFilterDialogFragment extends DialogFragment implements DatePick
 
 
 
+
+
     // handle the date selected
 
     @Override
-
     public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
         // store the values selected into a Calendar instance
         final Calendar c = Calendar.getInstance();
         c.set(Calendar.YEAR, year);
         c.set(Calendar.MONTH, monthOfYear);
         c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
+        // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+    }
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
     }
 
 }
